3b8754dd01c94e2188dcd517179e6e247dd30fe3
Bakterio <bakterio.2020@gmail.com>
2021-08-29 11:28:53 +0200

    MemberJoinLeaveEvent

---
+++ src/main/java/org/endev/elpatron/events/MemberJoinLeaveEvent.java
@@ -0,0 +1,34 @@
+package org.endev.elpatron.events;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.endev.elpatron.Config;
+
+import java.util.Random;
+
+public class MemberJoinLeaveEvent extends ListenerAdapter {
+
+    private final String[] welcomeMessages = { // TODO Add here some welcome messages.
+        "Vítej [member], doufáme že se ti tu bde líbit."
+    };
+
+    private final String[] leaveMessages = { // TODO Add here some welcome messages.
+            "[member] odešel, srab."
+    };
+
+    private Random r = new Random();
+
+    public void onGuildMemberJoin(GuildMemberJoinEvent e) {
+        Member m = e.getMember();
+
+        Config.WELCOME_CHANNEL.sendMessage(welcomeMessages[r.nextInt(welcomeMessages.length)].replaceFirst("[member]", m.getEffectiveName())).queue();
+    }
+
+    public void onGuildMemberRemove(GuildMemberRemoveEvent e) {
+        Member m = e.getMember();
+
+        Config.WELCOME_CHANNEL.sendMessage(welcomeMessages[r.nextInt(leaveMessages.length)].replaceFirst("[member]", m.getEffectiveName())).queue();
+    }
+}
